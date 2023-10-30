1c57358f9c05f800de4063eab7cd015ceb4a9fe5
azimny86 <azimny86@gmail.com>
2023-10-30 16:44:05 +0000

    Add support for django_summernote in the admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Event, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields:('description')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('body', 'post', 'created', 'approved')
+    list_filter = ('approved', 'created')
+    search_fields = ('email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
